ecd6bab2a57ff00c010f3bcb2793d2117c8b0a6b
pallavipatekar23 <pallavi.patekar23@gmail.com>
2024-12-17 17:42:28 +0530

    new code

--- Test Sample/src/SampleProject/ArraySplit.java
+++ Test Sample/src/SampleProject/ArraySplit.java
@@ -3,19 +3,29 @@ package SampleProject;
 public class ArraySplit {
 
 	public static void main(String[] args) {
-		String s = "Pallavi Bapu Patekar" ;
-		String[] splitname = s.split(" ");
-		System.out.println(splitname[0]);
-		System.out.println(splitname[2]);
-		System.out.println(splitname[1]);
+//		String s = "Pallavi Bapu Patekar" ;
+//		String[] splitname = s.split(" ");
+//		System.out.println(splitname[0]);
+//		System.out.println(splitname[2]);
+//		System.out.println(splitname[1]);
+//		
+//		String s1 = "Aditya Prakash Gupte Prakash Anita" ;
+//		String[] splitstring = s1.split("Prakash");
+//		System.out.println(splitstring[0].trim());
+//		System.out.println(splitstring[1].trim());
+//		System.out.println(splitstring[2].trim());
 		
-		String s1 = "Aditya Prakash Gupte Prakash Anita" ;
-		String[] splitstring = s1.split("Prakash");
-		System.out.println(splitstring[0].trim());
-		System.out.println(splitstring[1].trim());
-		System.out.println(splitstring[2].trim());
-		
-		
+		String r = "I am leaving this job";
+		for (int i=r.length()-1;i>=0;i--)
+		{
+		System.out.println(r.charAt(i));
+	}}
+		public int getUserData()
+		{
+			System.out.println("kay karat aahes?");
+			return 0;
+		}
 	}
 
-}
+
+
