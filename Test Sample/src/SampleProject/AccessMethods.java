6939b27060e6c601d30a84e291433ffc28ff9f93
pallavipatekar23 <pallavi.patekar23@gmail.com>
2024-12-17 17:42:17 +0530

    added new change

---
+++ Test Sample/src/SampleProject/AccessMethods.java
@@ -0,0 +1,28 @@
+package SampleProject;
+
+public class AccessMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AccessMethods d = new AccessMethods();
+		String name = d.getData();
+		System.out.println(name);
+		int i =d.getData1();
+		System.out.println(i);
+		ArraySplit p = new ArraySplit();
+		p.getUserData(); // getting this method from ArraySplit
+	}
+	
+	public String getData()
+	{
+		System.out.println("Hello");
+		return "pallavi";
+	}
+	public int getData1()
+	{
+		//System.out.println("Hello");
+		return 123;
+	}
+
+}
