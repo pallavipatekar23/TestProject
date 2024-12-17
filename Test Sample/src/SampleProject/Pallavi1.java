61b8f2cd07763a55b76878bd6aa34e923637ef27
pallavipatekar23 <pallavi.patekar23@gmail.com>
2024-12-17 11:19:53 +0530

    added array split program

--- Test Sample/src/SampleProject/Pallavi1.java
+++ Test Sample/src/SampleProject/Pallavi1.java
@@ -3,7 +3,19 @@ package SampleProject;
 public class Pallavi1 {
 
 	public static void main(String[] args) {
-System.out.println("Hello1");
+		String s = "Pallavi Bapu Patekar" ;
+		String[] splitname = s.split(" ");
+		System.out.println(splitname[0]);
+		System.out.println(splitname[2]);
+		System.out.println(splitname[1]);
+		
+		String s1 = "Aditya Prakash Gupte Prakash Anita" ;
+		String[] splitstring = s1.split("Prakash");
+		System.out.println(splitstring[0].trim());
+		System.out.println(splitstring[1].trim());
+		System.out.println(splitstring[2].trim());
+		
+		
 	}
 
 }
